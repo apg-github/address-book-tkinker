759c5344e5c18b9550753d5e64eb52c2f4f8910a
APG <apg.prywatny@gmail.com>
2021-03-21 16:40:16 +0100

    1# fully functional app without file reading

--- address-book.py
+++ address-book.py
@@ -8,26 +8,40 @@ from tkinter import messagebox as msb
 defaultAddressList = [['Chris', 'Meyers', '241-343-4349'],
                       ['Robert', 'Smith', '202-689-1234'],
                       ['Janet', 'Jones', '609-483-5432'],
-                      ['Ralph', 'Barnhart', '215-683-2341'],
+                      ['5QWQ', '5', '215-683-2341'],
                       ['Eric', 'Nelson', '571-485-2689'],
                       ['Ford', 'Prefect', '703-987-6543'],
                       ['Mary', 'Zigler', '812-567-8901'],
-                      ['Bob', 'Smith', '856-689-1234'],
-                      ['Bob', 'Smith', '856-689-1234'],
-                      ['Bob', 'Smith', '856-689-1234'],
-                      ['Bob', 'Smith', '856-689-1234']]
+                      ['Ciupa', 'Smith', '856-689-1234'],
+                      ['Drake', 'Smidth', '856-689-1234'],
+                      ['Movement', 'Szyn', '856-689-1234'],
+                      ['Bob', 'Ukol', '856-689-1234']]
 
 
 def which_selected():
     return int(select.curselection()[0])
 
 
-def add_entry():
+def clear_inputs():
+    fnamevar.set('')
+    fname.focus()
+    lnamevar.set('')
+    phonevar.set('')
+
+
+# paramter self=None jest konieczny w związku z bindowaniem entera do odpalenia tej funkcji, zgodnie z wątkiem w linku
+# https://stackoverflow.com/questions/43839536/typeerror-generatecode-takes-0-positional-arguments-but-1-was-given/43839602
+def add_entry(self=None):
     try:
-        if len(fnamevar.get()) == 0 or len(lnamevar.get()) == 0 or len(phonevar.get()):
+        if len(fnamevar.get()) == 0:
+            raise ValueError
+        elif len(lnamevar.get()) == 0:
+            raise ValueError
+        elif len(phonevar.get()) == 0:
             raise ValueError
         defaultAddressList.append([fnamevar.get(), lnamevar.get(), phonevar.get()])
         set_select()
+        clear_inputs()
     except ValueError:
         msb.showinfo(title='Dodawanie wpisu', message='Wypełnij wszystkie pola aby dodać wpis do listy.')
         pass
@@ -35,19 +49,27 @@ def add_entry():
 
 def update_entry():
     try:
-        if len(fnamevar.get()) == 0 or len(lnamevar.get()) == 0 or len(phonevar.get()) or which_selected() == 0:
+        if len(fnamevar.get()) == 0:
             raise ValueError
-        defaultAddressList[which_selected()] = [fnamevar.get(),
-                                                lnamevar.get(),
-                                                phonevar.get()]
+        elif len(lnamevar.get()) == 0:
+            raise ValueError
+        elif len(phonevar.get()) == 0:
+            raise ValueError
+        elif which_selected() < 0:
+            raise ValueError
+        defaultAddressList[which_selected()] = [fnamevar.get(), lnamevar.get(), phonevar.get()]
+        set_select()
+        clear_inputs()
     except ValueError:
         msb.showinfo(title='Aktualizowanie wpisu', message='Zaznacz wpis oraz uzupełnij wszystkie pola aby zaktualizować wpis.')
         pass
 
 
-def delete_entry():
+# paramter self=None jest konieczny w związku z bindowaniem entera do odpalenia tej funkcji, zgodnie z wątkiem w linku
+# https://stackoverflow.com/questions/43839536/typeerror-generatecode-takes-0-positional-arguments-but-1-was-given/43839602
+def delete_entry(self=None):
     try:
-        if which_selected() == 0:
+        if which_selected() < 0:
             raise Exception
         del defaultAddressList[which_selected()]
         set_select()
@@ -58,38 +80,38 @@ def delete_entry():
 
 def load_entry():
     try:
-        if which_selected() == 0:
+        if which_selected() < 0:
             raise Exception
         fname, lname, phone = defaultAddressList[which_selected()]
         fnamevar.set(fname)
         lnamevar.set(lname)
         phonevar.set(phone)
     except Exception:
-        msb.showinfo(title='Załadowanie wpisu', message='Musisz zaznaczyć wpis aby go go załadować do inputów.')
+        msb.showinfo(title='Załadowanie wpisu', message='Musisz zaznaczyć wpis aby go załadować do inputów.')
         pass
 
 
 def make_window():
-    global fnamevar, lnamevar, phonevar, select
+    global fnamevar, lnamevar, phonevar, select, fname
 
     win = Tk()
     win.title('Prosta książka adresowa')
     frame1 = Frame(win)
     frame1.pack(padx=30, pady=30)
 
-    Label(frame1, text="Imię").grid(row=0, column=0, sticky=W, padx=5)
+    Label(frame1, text="Imię", width=20).grid(row=0, column=0, sticky=W, padx=5)
     fnamevar = StringVar()
-    fname = Entry(frame1, textvariable=fnamevar)
-    fname.grid(row=0, column=1, sticky=W,padx=10, pady=10)
+    fname = Entry(frame1, textvariable=fnamevar, width=50)
+    fname.grid(row=0, column=1, sticky=W, padx=10, pady=10)
 
-    Label(frame1, text="Nazwisko").grid(row=1, column=0, sticky=W,padx=5)
+    Label(frame1, text="Nazwisko", width=20).grid(row=1, column=0, sticky=W, padx=5)
     lnamevar = StringVar()
-    lname = Entry(frame1, textvariable=lnamevar)
+    lname = Entry(frame1, textvariable=lnamevar, width=50)
     lname.grid(row=1, column=1, sticky=W, padx=10, pady=10)
 
-    Label(frame1, text="Telefon").grid(row=2, column=0, sticky=W, padx=5)
+    Label(frame1, text="Telefon", width=20).grid(row=2, column=0, sticky=W, padx=5)
     phonevar = StringVar()
-    phone = Entry(frame1, textvariable=phonevar)
+    phone = Entry(frame1, textvariable=phonevar, width=50)
     phone.grid(row=2, column=1, sticky=W, padx=10, pady=10)
 
     frame2 = Frame(win)
@@ -99,7 +121,7 @@ def make_window():
     b1['font'] = font
     b2 = Button(frame2, text="Zaktualizuj wybrany", command=update_entry)
     b2['font'] = font
-    b3 = Button(frame2, text="Usuń wybrany", command=delete_entry , fg='red')
+    b3 = Button(frame2, text="Usuń wybrany", command=delete_entry, fg='red')
     b3['font'] = font
     b4 = Button(frame2, text="Załaduj wybrany", command=load_entry)
     b4['font'] = font
@@ -118,27 +140,15 @@ def make_window():
     scroll.config(command=select.yview)
     scroll.pack(side=RIGHT, fill=Y)
     select.pack(side=LEFT, fill=BOTH, expand=1)
-
     return win
 
 
 def set_select():
     defaultAddressList.sort(key=lambda record: record[1])
     select.delete(0, END)
-    try:
-        f = open("address-list.txt", "r")
-        print(f.read())
-        values = f.read()
-        for fname, lname, phone in values:
-            select.insert(END, "{0}, {1}, {2}".format(lname, fname, phone))
-        selectIndex = select.index("end")
-        if selectIndex == 0:
-            f.close()
-            print("The listbox cannot be loaded from a file. Using default list instead.")
-            raise Exception
-    except Exception:
-        for fname, lname, phone in defaultAddressList:
-            select.insert(END, "{0}, {1}, {2}".format(lname, fname, phone))
+    clear_inputs()
+    for fname, lname, phone in defaultAddressList:
+        select.insert(END, "{0}, {1}, {2}".format(lname, fname, phone))
 
 
 win = make_window()
@@ -146,5 +156,6 @@ set_select()
 win.geometry('900x600')
 win.configure(background='white')
 win.eval('tk::PlaceWindow . center')
+win.bind('<Return>', add_entry)
+win.bind('<Delete>', delete_entry)
 win.mainloop()
-
